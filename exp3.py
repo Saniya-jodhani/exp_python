2ed0fce0e97232613eb43473d92090203913978d
Saniya-jodhani <varshajodhani588@gmail.com>
2026-01-21 10:30:54 +0530

    Add files via upload
    exp3

---
+++ exp3.py
@@ -0,0 +1,14 @@
+# check whether given number is an even or not
+
+num = int(input("Enter a number: "))
+if num%2 ==0:
+    print(f"{num} is even number")
+else:
+    print(f"{num} is odd number")
+
+
+# a program to print the decimal euivalents
+
+for i in range(2, 11):
+    print("1/",i, "=", 1/i )
+
